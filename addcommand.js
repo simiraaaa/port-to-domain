187dad2da3dd4b6af504971d2a9df356090d3cfe
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2017-11-24 12:23:55 +0900

    addcommandで{PORT}を置換しない

--- addcommand.js
+++ addcommand.js
@@ -5,7 +5,7 @@ const command = process.argv[3];
 const envdir = process.argv[4]|| process.argv[1].replace(/[^/]*$/, '');
 
 const server = settings.servers[app];
-server.command = command.replace('{PORT}', server.port);
+server.command = command;
 server.envdir = envdir;
 
 require('fs').writeFileSync('./settings.json', JSON.stringify(settings, null, '  '));
